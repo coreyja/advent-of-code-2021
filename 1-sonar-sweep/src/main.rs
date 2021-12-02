ebe2c1a3202dd547e0381d443d4425b61dec193b
Corey Alexander <coreyja@gmail.com>
2021-12-02 12:13:07 -0500

    Day 1 Part A

--- 1-sonar-sweep/src/main.rs
+++ 1-sonar-sweep/src/main.rs
@@ -1,3 +1,30 @@
+use itertools::Itertools;
+
+fn count_increases(lines: Vec<u64>) -> usize {
+    lines.iter().tuple_windows().filter(|(a, b)| a < b).count()
+}
+
+fn transform_input(file: &str) -> Vec<u64> {
+    file.lines()
+        .map(|line| line.parse::<u64>().unwrap())
+        .collect()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let sample_input: Vec<u64> = transform_input(include_str!("sample.input"));
+    let my_input: Vec<u64> = transform_input(include_str!("my.input"));
+
+    println!("Sample Answer: {}", count_increases(sample_input));
+    println!("My Answer: {}", count_increases(my_input));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_sample() {
+        let sample_input: Vec<u64> = transform_input(include_str!("sample.input"));
+        assert_eq!(count_increases(sample_input), 7);
+    }
 }
