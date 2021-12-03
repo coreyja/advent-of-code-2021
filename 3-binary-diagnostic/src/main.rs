f0d3a39035711460312766e38f94c88c741215e4
Corey Alexander <coreyja@gmail.com>
2021-12-03 11:05:38 -0500

    Day 3 Part 2

--- 3-binary-diagnostic/src/main.rs
+++ 3-binary-diagnostic/src/main.rs
@@ -1,5 +1,48 @@
 use itertools::Itertools;
 
+fn into_vec(s: &str) -> Vec<Vec<char>> {
+    s.lines().map(|l| l.chars().collect_vec()).collect_vec()
+}
+
+enum MostOrLeastCommon {
+    Most,
+    Least,
+}
+
+fn filter_common(mut v: Vec<Vec<char>>, filter_on: MostOrLeastCommon) -> Vec<char> {
+    let mut current_digit = 0;
+
+    while v.len() > 1 {
+        let mut digits = vec![];
+
+        for line in v.iter() {
+            digits.push(line[current_digit]);
+        }
+
+        let ((least, least_count), (most, most_count)) = least_and_most_common_with_counts(digits);
+        let matching_digit = if least_count == most_count {
+            match filter_on {
+                MostOrLeastCommon::Most => '1',
+                MostOrLeastCommon::Least => '0',
+            }
+        } else {
+            match filter_on {
+                MostOrLeastCommon::Most => most,
+                MostOrLeastCommon::Least => least,
+            }
+        };
+
+        v = v
+            .into_iter()
+            .filter(|line| line[current_digit] == matching_digit)
+            .collect_vec();
+
+        current_digit += 1;
+    }
+
+    v.first().unwrap().clone()
+}
+
 fn into_digit_position_vecs(s: &str) -> Vec<Vec<char>> {
     let mut lines = s.lines().map(|l| l.chars()).collect_vec();
     let mut result = Vec::new();
@@ -22,16 +65,22 @@ fn into_digit_position_vecs(s: &str) -> Vec<Vec<char>> {
     result
 }
 
-fn least_and_most_common(v: Vec<char>) -> (char, char) {
+fn least_and_most_common_with_counts(v: Vec<char>) -> ((char, usize), (char, usize)) {
     let counts = v.into_iter().counts();
 
     let counts_v: Vec<_> = counts.into_iter().collect_vec();
 
     let (min, max) = counts_v
-        .iter()
+        .into_iter()
         .minmax_by_key(|x| x.1)
         .into_option()
         .unwrap();
+
+    (min, max)
+}
+
+fn least_and_most_common(v: Vec<char>) -> (char, char) {
+    let (min, max) = least_and_most_common_with_counts(v);
 
     (min.0, max.0)
 }
@@ -62,10 +111,30 @@ fn part1_ans(s: &str) -> i64 {
     epsilon_num * gamma_num
 }
 
+fn part2_ans(s: &str) -> i64 {
+    let v = into_vec(s);
+
+    let oxygen: String = filter_common(v.clone(), MostOrLeastCommon::Most)
+        .into_iter()
+        .collect();
+    let c02: String = filter_common(v, MostOrLeastCommon::Least)
+        .into_iter()
+        .collect();
+
+    let oxygen_num = i64::from_str_radix(&oxygen, 2).unwrap();
+    let c02_num = i64::from_str_radix(&c02, 2).unwrap();
+
+    oxygen_num * c02_num
+}
+
 fn main() {
     println!("Part 1");
     println!("Sample: {:?}", part1_ans(include_str!("sample.input")));
     println!("My: {:?}", part1_ans(include_str!("my.input")));
+
+    println!("Part 2");
+    println!("Sample: {:?}", part2_ans(include_str!("sample.input")));
+    println!("My: {:?}", part2_ans(include_str!("my.input")));
 }
 
 #[cfg(test)]
@@ -89,5 +158,17 @@ mod tests {
     #[test]
     fn test_least_and_most_common() {
         assert_eq!(('1', '0'), least_and_most_common(vec!['0', '1', '0', '0']));
+    }
+
+    #[test]
+    fn test_part_1() {
+        assert_eq!(198, part1_ans(include_str!("sample.input")));
+        assert_eq!(2250414, part1_ans(include_str!("my.input")));
+    }
+
+    #[test]
+    fn test_part_2() {
+        assert_eq!(230, part2_ans(include_str!("sample.input")));
+        assert_eq!(6085575, part2_ans(include_str!("my.input")));
     }
 }
