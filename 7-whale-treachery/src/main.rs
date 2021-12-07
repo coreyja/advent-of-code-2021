07c944e0e74299d8f85c1b6587311c3b19d60f8f
Corey Alexander <coreyja@gmail.com>
2021-12-07 11:54:46 -0500

    Day 7 Part 2

--- 7-whale-treachery/src/main.rs
+++ 7-whale-treachery/src/main.rs
@@ -29,11 +29,15 @@ fn diff_u32(a: u32, b: u32) -> u32 {
     }
 }
 
-fn sum_of_squares_diff(nums: &[u32], index: u32) -> u32 {
+fn addition_up_to(n: u32) -> u32 {
+    (1..=n).sum()
+}
+
+fn sum_of_additions_diff(nums: &[u32], index: u32) -> u32 {
     let mut diff: u32 = 0;
 
     for n in nums {
-        diff += diff_u32(*n, index).pow(2);
+        diff += addition_up_to(diff_u32(*n, index));
     }
 
     diff
@@ -64,10 +68,29 @@ fn part1_ans(s: &str) -> Result<(usize, u32)> {
         .ok_or_else(|| anyhow!("No min found"))
 }
 
+fn part2_ans(s: &str) -> Result<(usize, u32, u32)> {
+    let nums = parse_input(s)?;
+
+    (0..nums.len())
+        .map(|i| {
+            (
+                i,
+                sum_of_additions_diff(&nums, i as u32),
+                sum_of_diff(&nums, i as u32),
+            )
+        })
+        .min_by_key(|(i, x, _)| *x)
+        .ok_or_else(|| anyhow!("No min found"))
+}
+
 fn main() -> Result<()> {
     println!("Part 1");
     println!("Sample: {:?}", part1_ans(include_str!("sample.input"))?);
     println!("My: {:?}", part1_ans(include_str!("my.input"))?);
+
+    println!("Part 2");
+    println!("Sample: {:?}", part2_ans(include_str!("sample.input"))?);
+    println!("My: {:?}", part2_ans(include_str!("my.input"))?);
 
     Ok(())
 }
