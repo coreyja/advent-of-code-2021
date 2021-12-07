b37f121bd2467f9162a78693d4a577a57764d7ff
Corey Alexander <coreyja@gmail.com>
2021-12-07 11:47:43 -0500

    Day 7 Part 1

--- 7-whale-treachery/src/main.rs
+++ 7-whale-treachery/src/main.rs
@@ -1,4 +1,5 @@
 use anyhow::{anyhow, Result};
+use itertools::Itertools;
 
 fn parse_input(s: &str) -> Result<Vec<u32>> {
     s.trim()
@@ -20,10 +21,53 @@ fn median(array: &Vec<u32>) -> f64 {
     }
 }
 
-fn main() -> Result<()> {
-    let nums = parse_input(include_str!("sample.input"))?;
+fn diff_u32(a: u32, b: u32) -> u32 {
+    if a > b {
+        a - b
+    } else {
+        b - a
+    }
+}
 
-    dbg!(median(&nums));
+fn sum_of_squares_diff(nums: &[u32], index: u32) -> u32 {
+    let mut diff: u32 = 0;
+
+    for n in nums {
+        diff += diff_u32(*n, index).pow(2);
+    }
+
+    diff
+}
+
+fn sum_of_diff(nums: &[u32], index: u32) -> u32 {
+    let mut diff: u32 = 0;
+
+    for n in nums {
+        diff += diff_u32(*n, index);
+    }
+
+    diff
+}
+
+// fn least_squares_index(nums: &[u32]) -> (usize, u32) {
+//     dbg!((0..*nums.iter().max().unwrap())
+//         .map(|i| (i, sum_of_diff(nums, i as u32)))
+//         .collect_vec());
+// }
+
+fn part1_ans(s: &str) -> Result<(usize, u32)> {
+    let nums = parse_input(s)?;
+
+    (0..nums.len())
+        .map(|i| (i, sum_of_diff(&nums, i as u32)))
+        .min_by_key(|(i, x)| *x)
+        .ok_or_else(|| anyhow!("No min found"))
+}
+
+fn main() -> Result<()> {
+    println!("Part 1");
+    println!("Sample: {:?}", part1_ans(include_str!("sample.input"))?);
+    println!("My: {:?}", part1_ans(include_str!("my.input"))?);
 
     Ok(())
 }
