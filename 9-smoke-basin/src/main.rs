05213ee91520eb6c6b6cb04b43d8b0a18e51a92f
Corey Alexander <coreyja@gmail.com>
2021-12-09 00:59:49 -0500

    Day 9 Part 2

--- 9-smoke-basin/src/main.rs
+++ 9-smoke-basin/src/main.rs
@@ -12,7 +12,7 @@ impl Board {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Pos(i32, i32);
 
 impl Pos {
@@ -45,6 +45,30 @@ impl Pos {
                 < board.cells[neighbour.0 as usize][neighbour.1 as usize]
         })
     }
+
+    fn find_basin_size(&self, board: &Board) -> u32 {
+        let mut visited = vec![vec![false; board.cells[0].len()]; board.cells.len()];
+        let mut queue = vec![self.clone()];
+        let mut size = 0;
+
+        while let Some(pos) = queue.pop() {
+            if visited[pos.0 as usize][pos.1 as usize] {
+                continue;
+            }
+            visited[pos.0 as usize][pos.1 as usize] = true;
+
+            let value = board.get(&pos);
+
+            if value != 9 {
+                size += 1;
+                for n in pos.get_neighbours(board).into_iter() {
+                    queue.push(n);
+                }
+            }
+        }
+
+        size
+    }
 }
 
 fn parse_input(s: &str) -> Result<Board> {
@@ -64,13 +88,33 @@ fn parse_input(s: &str) -> Result<Board> {
     Ok(Board { cells })
 }
 
+fn part2_ans(s: &str) -> Result<u32> {
+    let board = parse_input(s)?;
+    let size_x = board.cells.len() as i32;
+    let size_y = board.cells[0].len() as i32;
+
+    let low = iproduct!((0..size_x), (0..size_y))
+        .map(|(x, y)| Pos::new(x, y))
+        .filter(|pos| pos.is_low_point(&board))
+        .collect_vec();
+
+    let mut basin_sizes = low
+        .iter()
+        .map(|pos| pos.find_basin_size(&board))
+        .collect_vec();
+
+    basin_sizes.sort_unstable();
+
+    Ok(basin_sizes[basin_sizes.len() - 3..].iter().product())
+}
+
 fn part1_ans(s: &str) -> Result<i32> {
     let board = parse_input(s)?;
 
-    let sizeX = board.cells.len() as i32;
-    let sizeY = board.cells[0].len() as i32;
+    let size_x = board.cells.len() as i32;
+    let size_y = board.cells[0].len() as i32;
 
-    let low = iproduct!((0..sizeX), (0..sizeY))
+    let low = iproduct!((0..size_x), (0..size_y))
         .map(|(x, y)| Pos::new(x, y))
         .filter(|pos| pos.is_low_point(&board))
         .collect_vec();
@@ -78,9 +122,13 @@ fn part1_ans(s: &str) -> Result<i32> {
     Ok(low.iter().map(|pos| board.get(pos)).sum::<i32>() + low.len() as i32)
 }
 fn main() -> Result<()> {
-    println!("Par 1");
+    println!("Part 1");
     println!("Sample: {}", part1_ans(include_str!("sample.input"))?);
     println!("My: {}", part1_ans(include_str!("my.input"))?);
+
+    println!("Part 2");
+    println!("Sample: {}", part2_ans(include_str!("sample.input"))?);
+    println!("My: {}", part2_ans(include_str!("my.input"))?);
 
     Ok(())
 }
