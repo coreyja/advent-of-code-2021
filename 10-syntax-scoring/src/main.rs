22849bbdb3c57ef166ea91cd416035ad62a37368
Corey Alexander <coreyja@gmail.com>
2021-12-10 00:28:15 -0500

    Day 10 Part 2

--- 10-syntax-scoring/src/main.rs
+++ 10-syntax-scoring/src/main.rs
@@ -16,6 +16,15 @@ fn score_invalid(c: &char) -> u64 {
         _ => panic!("invalid char"),
     }
 }
+fn score_valid(c: &char) -> u64 {
+    match c {
+        ')' => 1,
+        ']' => 2,
+        '}' => 3,
+        '>' => 4,
+        _ => panic!("invalid char"),
+    }
+}
 
 fn fist_invalid_character(line: &str) -> Option<char> {
     let mut stack = vec![];
@@ -37,16 +46,70 @@ fn fist_invalid_character(line: &str) -> Option<char> {
     None
 }
 
+fn parse_line(line: &str) -> Result<Vec<char>, char> {
+    let mut stack = vec![];
+
+    for character in line.chars() {
+        match character {
+            '(' | '{' | '[' | '<' => stack.push(character),
+            ')' | '}' | ']' | '>' => {
+                if stack.last().map(close_char) == Some(character) {
+                    stack.pop();
+                } else {
+                    return Err(character);
+                }
+            }
+            _ => panic!("invalid character"),
+        }
+    }
+
+    Ok(stack)
+}
+
 fn part1_ans(s: &str) -> u64 {
     let input = s.trim().lines();
 
-    let corrupt = input.filter_map(fist_invalid_character).collect::<Vec<_>>();
+    let corrupt = input
+        .map(parse_line)
+        .filter_map(|result| result.err())
+        .collect::<Vec<_>>();
 
     corrupt.iter().map(score_invalid).sum()
+}
+
+fn part2_ans(s: &str) -> u64 {
+    let input = s.trim().lines();
+
+    let mut scores = input
+        .map(parse_line)
+        .filter_map(|result| result.ok())
+        .map(|mut stack| {
+            stack.reverse();
+            stack.iter().map(close_char).collect::<Vec<_>>()
+        })
+        .map(|stack| {
+            let mut score = 0;
+
+            for c in stack {
+                score *= 5;
+                score += score_valid(&c);
+            }
+
+            score
+        })
+        .collect::<Vec<_>>();
+
+    scores.sort();
+
+    scores[scores.len() / 2]
 }
 
 fn main() {
     println!("Part 1");
     println!("Sample: {}", part1_ans(include_str!("sample.input")));
-    println!("Sample: {}", part1_ans(include_str!("my.input")));
+    println!("My: {}", part1_ans(include_str!("my.input")));
+
+    println!("Part 2");
+    println!("Sample: {}", part2_ans(include_str!("sample.input")));
+    println!("My: {}", part2_ans(include_str!("my.input")));
 }
